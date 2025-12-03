0ef035e387e2cddb96af2389c04d34cc710dac09
Ayshi Paul Asha <ayshipaul00@gmail.com>
2025-12-04 00:01:28 +0600

    Update and rename Insert fast.c to Insert first.c

--- Insert first.c
+++ Insert first.c
@@ -3,15 +3,16 @@ int main()
 {
     int arr[6]={2,3,4,5,6};
     int n=5;
-    int value=1;
+    int ITEM=1;
     for(int i=n; i>0; i--)
     {
         arr[i] = arr[i-1];
     }
-     arr[0]=value;
+     arr[0]=ITEM;
    for(int i=0; i<n+1; i++)
     {
         printf("%d\n", arr[i]);
     }
    return 0;
 }
+
